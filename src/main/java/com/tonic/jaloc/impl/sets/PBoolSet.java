049b8fda06be9784e53796aab093de26b5939613
TonicBox <73169574+Tonic-Box@users.noreply.github.com>
2026-07-20 09:16:35 -0500

    Add fixed-domain bitmap sets for byte, short, char, and bool

---
+++ src/main/java/com/tonic/jaloc/impl/sets/PBoolSet.java
@@ -0,0 +1,82 @@
+package com.tonic.jaloc.impl.sets;
+
+import com.tonic.jaloc.impl.arrays.PLongArray;
+import com.tonic.jaloc.impl.arrays.PLongWriter;
+import com.tonic.jaloc.memory.SystemAllocator;
+import com.tonic.jaloc.memory.abs.AbstractNativeCollection;
+import com.tonic.jaloc.memory.iface.NativeAllocator;
+
+public final class PBoolSet extends AbstractNativeCollection<PLongArray, PLongWriter>
+{
+    private static final long WORDS = 1;
+
+    public PBoolSet()
+    {
+        this(SystemAllocator.getInstance());
+    }
+
+    public PBoolSet(NativeAllocator allocator)
+    {
+        super(allocator, new PLongArray(allocator, WORDS));
+    }
+
+    @Override
+    protected PLongArray createArray(NativeAllocator allocator, long capacity)
+    {
+        return new PLongArray(allocator, capacity);
+    }
+
+    @Override
+    protected void migrateElements(PLongArray source, PLongArray destination)
+    {
+        long words = source.length();
+
+        for (long i = 0; i < words; i++)
+        {
+            destination.set(i, source.get(i));
+        }
+    }
+
+    public boolean add(boolean value)
+    {
+        long mask = 1L << (value ? 1 : 0);
+        long current = elements().get(0);
+
+        if ((current & mask) != 0)
+        {
+            return false;
+        }
+
+        elements().set(0, current | mask);
+        size(size() + 1);
+        return true;
+    }
+
+    public boolean remove(boolean value)
+    {
+        long mask = 1L << (value ? 1 : 0);
+        long current = elements().get(0);
+
+        if ((current & mask) == 0)
+        {
+            return false;
+        }
+
+        elements().set(0, current & ~mask);
+        size(size() - 1);
+        return true;
+    }
+
+    public boolean contains(boolean value)
+    {
+        return (elements().get(0) & (1L << (value ? 1 : 0))) != 0;
+    }
+
+    public void clear()
+    {
+        ensureOpen();
+
+        elements().clear();
+        size(0);
+    }
+}
---
+++ src/main/java/com/tonic/jaloc/impl/sets/PCharSet.java
@@ -0,0 +1,111 @@
+package com.tonic.jaloc.impl.sets;
+
+import com.tonic.jaloc.impl.arrays.PLongArray;
+import com.tonic.jaloc.impl.arrays.PLongWriter;
+import com.tonic.jaloc.memory.SystemAllocator;
+import com.tonic.jaloc.memory.abs.AbstractNativeCollection;
+import com.tonic.jaloc.memory.iface.NativeAllocator;
+
+import java.util.Objects;
+import java.util.function.IntConsumer;
+
+public final class PCharSet extends AbstractNativeCollection<PLongArray, PLongWriter>
+{
+    private static final long WORDS = 1024;
+
+    public PCharSet()
+    {
+        this(SystemAllocator.getInstance());
+    }
+
+    public PCharSet(NativeAllocator allocator)
+    {
+        super(allocator, new PLongArray(allocator, WORDS));
+    }
+
+    @Override
+    protected PLongArray createArray(NativeAllocator allocator, long capacity)
+    {
+        return new PLongArray(allocator, capacity);
+    }
+
+    @Override
+    protected void migrateElements(PLongArray source, PLongArray destination)
+    {
+        long words = source.length();
+
+        for (long i = 0; i < words; i++)
+        {
+            destination.set(i, source.get(i));
+        }
+    }
+
+    public boolean add(char value)
+    {
+        long index = value;
+        long word = index >>> 6;
+        long mask = 1L << index;
+        long current = elements().get(word);
+
+        if ((current & mask) != 0)
+        {
+            return false;
+        }
+
+        elements().set(word, current | mask);
+        size(size() + 1);
+        return true;
+    }
+
+    public boolean remove(char value)
+    {
+        long index = value;
+        long word = index >>> 6;
+        long mask = 1L << index;
+        long current = elements().get(word);
+
+        if ((current & mask) == 0)
+        {
+            return false;
+        }
+
+        elements().set(word, current & ~mask);
+        size(size() - 1);
+        return true;
+    }
+
+    public boolean contains(char value)
+    {
+        long index = value;
+
+        return (elements().get(index >>> 6) & (1L << index)) != 0;
+    }
+
+    public void forEach(IntConsumer consumer)
+    {
+        Objects.requireNonNull(consumer, "consumer");
+
+        long words = capacity();
+
+        for (long word = 0; word < words; word++)
+        {
+            long current = elements().get(word);
+
+            while (current != 0)
+            {
+                long bit = Long.numberOfTrailingZeros(current);
+
+                consumer.accept((char) ((word << 6) + bit));
+                current &= current - 1;
+            }
+        }
+    }
+
+    public void clear()
+    {
+        ensureOpen();
+
+        elements().clear();
+        size(0);
+    }
+}
